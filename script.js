7ee3b8e3b124cf9c48d3045d29c7d7f0ebda5808
RaihanLaisa <leaderraihan@gmail.com>
2024-03-27 17:15:08 +0700

    ganti

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
-let move_speed = 3, grativy = 0.28;
+let move_speed = 10, grativy = 0.28;
 let bird = document.querySelector('.bird');
 let img = document.getElementById('bird-1');
 let sound_point = new Audio('sounds effect/point.mp3');
 let sound_die = new Audio('sounds effect/die.mp3');
 
-// getting bird element properties
 let bird_props = bird.getBoundingClientRect();
 
-// This method returns DOMReact -> top, right, bottom, left, x, y, width and height
 let background = document.querySelector('.background').getBoundingClientRect();
 
 let score_val = document.querySelector('.score_val');
@@ -105,7 +103,7 @@ function play(){
         if(game_state != 'Play') return;
 
         if(pipe_seperation > 115){
-            pipe_seperation = 0;
+            pipe_seperation = 70;
 
             let pipe_posi = Math.floor(Math.random() * 43) + 8;
             let pipe_sprite_inv = document.createElement('div');
